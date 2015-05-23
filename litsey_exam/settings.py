71e09e133ba43617f784ea306955605cb60775f2
Shuhrat Azimuratov <shuhrat94@ya.ru>
2015-05-23 12:39:17 +0300

    ss

--- litsey_exam/settings.py
+++ litsey_exam/settings.py
@@ -59,24 +59,24 @@ WSGI_APPLICATION = 'litsey_exam.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 #lyceedja
-# DATABASES = {
-#  'default': {
-#  'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#  'NAME': 'lyceedja_journal', # Or path to database file if using sqlite3.
-#  'USER': 'lyceedja_user', # Not used with sqlite3.
-#  'PASSWORD': '123456789', # Not used with sqlite3.
-#  'HOST': '127.0.0.1', # Set to empty string for localhost. Not used with sqlite3.
-#  'PORT': '', # Set to empty string for default. Not used with sqlite3.
-#  }
-# }
+DATABASES = {
+ 'default': {
+ 'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+ 'NAME': 'd9orkamdai16db', # Or path to database file if using sqlite3.
+ 'USER': 'wvoryagqrfwfoh', # Not used with sqlite3.
+ 'PASSWORD': 'FeMJfHHsuBsIGQWx5Dzs9JHUgF', # Not used with sqlite3.
+ 'HOST': 'ec2-54-247-79-142.eu-west-1.compute.amazonaws.com', # Set to empty string for localhost. Not used with sqlite3.
+ 'PORT': '5432', # Set to empty string for default. Not used with sqlite3.
+ }
+}
 
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(PROJECT_DIR, 'db.sqlite3'),
+#     }
+# }
 
 #DATABASES = { 'default' : dj_database_url.config()}
 
