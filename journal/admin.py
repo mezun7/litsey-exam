cda3cd1532f2e95e06e49ac6b9b9a91525d725b5
Shuhrat Azimuratov <shuhrat94@ya.ru>
2015-05-26 13:11:18 +0300

    export to xlsx added

--- journal/admin.py
+++ journal/admin.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.contrib import admin
 
 # Register your models here.
@@ -5,27 +6,92 @@ from django.http import HttpResponse
 from journal.models import Teacher, Class, Subject, Mark, Student
 
 
+
+def export_xlsx(modeladmin, request, queryset):
+    import openpyxl
+    from django.utils.encoding import smart_str
+    from openpyxl.cell import get_column_letter
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=student-info.xlsx'
+    wb = openpyxl.Workbook()
+    ws = wb.get_active_sheet()
+    ws.title = u"Информация об учениках"
+
+    row_num = 0
+
+    columns = [
+        (u"ID", 15),
+        (u"Фамилия", 20),
+        (u"Имя", 20),
+        (u"Отчество", 20),
+        (u"Школа", 20),
+        (u"Класс", 5),
+        (u"Оплата питания", 15),
+        (u"Справка со школы", 10),
+        (u"Копия мед карты", 10),
+    ]
+
+    for col_num in xrange(len(columns)):
+        c = ws.cell(row=row_num + 1, column=col_num + 1)
+        c.value = columns[col_num][0]
+        c.style.font.bold = True
+        # set column width
+        ws.column_dimensions[get_column_letter(col_num+1)].width = columns[col_num][1]
+
+    for obj in queryset:
+        row_num += 1
+        row = [
+            obj.pk,
+            smart_str(obj.fname),
+            smart_str(obj.lname),
+            smart_str(obj.fathers_name),
+            smart_str(obj.school),
+            smart_str(obj.class_name),
+            obj.pay_for_eating,
+            smart_str(obj.report_from_school),
+            smart_str(obj.medical_card)
+
+
+        ]
+        for col_num in xrange(len(row)):
+            c = ws.cell(row=row_num + 1, column=col_num + 1)
+            c.value = row[col_num]
+            c.style.alignment.wrap_text = True
+
+    wb.save(response)
+    return response
+
+export_xlsx.short_description = u"Export XLSX"
+
 def export_csv(StudentAdmin, request, queryset):
     import csv
     from django.utils.encoding import smart_str
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=mymodel.csv'
+    response['Content-Disposition'] = 'attachment; filename=student_info.csv'
     writer = csv.writer(response, csv.excel)
     response.write(u'\ufeff'.encode('utf8')) # BOM (optional...Excel needs it to open UTF-8 file properly)
     writer.writerow([
         smart_str(u"ID"),
-        smart_str(u"Surname"),
-        smart_str(u"Name"),
-        smart_str(u'School'),
-        smart_str(u"Class"),
+        smart_str(u"Фамилия"),
+        smart_str(u"Имя"),
+        smart_str(u"Отчество"),
+        smart_str(u'Школа'),
+        smart_str(u"Класс"),
+        smart_str(u'Оплата питания'),
+        smart_str(u"Справка со школы"),
+        smart_str(u"Копия мед карты"),
     ])
     for obj in queryset:
         writer.writerow([
             smart_str(obj.pk),
             smart_str(obj.fname),
             smart_str(obj.lname),
+            smart_str(obj.fathers_name),
             smart_str(obj.school),
             smart_str(obj.class_name),
+            smart_str(obj.pay_for_eating),
+            smart_str(obj.report_from_school),
+            smart_str(obj.medical_card),
         ])
     return response
 export_csv.short_description = u"Export CSV"
@@ -38,8 +104,8 @@ class ClassAdmin(admin.ModelAdmin):
     list_display = ('name', 'class1_teacher')
     search_fields = ('name',)
 class StudentAdmin(admin.ModelAdmin):
-    actions = [export_csv,]
-    list_display = ('lname', 'fname', 'school', 'class_name')
+    actions = [export_csv, export_xlsx]
+    list_display = ('fname', 'lname', 'fathers_name', 'school', 'class_name')
     search_fields = ('lname', 'fname', 'school')
 
 class MarkAdmin(admin.ModelAdmin):
--- journal/models.py
+++ journal/models.py
@@ -1,49 +1,53 @@
+# coding=utf-8
 from django.contrib.auth.models import User
 from django.db import models
-
+# -*- coding: utf-8 -*-
 # Create your models here.
 
 
 class Subject(models.Model):
-    name = models.CharField(max_length=200)
-
+    name = models.CharField(max_length=200, verbose_name='Название предмета')
     def __unicode__(self):
         return self.name
 
 
 class Teacher(models.Model):
-    user = models.ForeignKey(User)
-    subject = models.ForeignKey(Subject)
+    user = models.ForeignKey(User, verbose_name=u'Пользователь')
+    subject = models.ForeignKey(Subject, verbose_name=u'Предмет')
 
     def __unicode__(self):
         return self.user.last_name + " " + self.user.first_name
 
 
 class Class(models.Model):
-    name = models.CharField(max_length=20)
-    class1_teacher = models.ForeignKey(User)
-    teacher = models.ManyToManyField(Teacher, blank=True)
+    name = models.CharField(max_length=20, verbose_name='Литер класса')
+    class1_teacher = models.ForeignKey(User, verbose_name=u'Классный руководитель')
+    teacher = models.ManyToManyField(Teacher, blank=True, verbose_name=u'Учителя класса')
 
     def __unicode__(self):
         return self.name
 
 
 class Student(models.Model):
-    fname = models.CharField(max_length=100)
-    lname = models.CharField(max_length=100)
-    school = models.CharField(max_length=100)
-    class_name = models.ForeignKey(Class)
-    photo = models.ImageField(blank=True, upload_to='avatars')
+    fname = models.CharField(max_length=100, verbose_name=(u'Фамилия'))
+    lname = models.CharField(max_length=100, verbose_name=(u'Имя'))
+    fathers_name = models.CharField(max_length=100, verbose_name=(u'Отчество'))
+    school = models.CharField(max_length=100, verbose_name=(u'Школа'))
+    class_name = models.ForeignKey(Class, verbose_name=(u'Класс'))
+    photo = models.ImageField(blank=True, upload_to='avatars', verbose_name=(u'Фото'))
     about = models.CharField(max_length=1000, blank=True)
+    pay_for_eating = models.IntegerField(verbose_name=(u'Оплата питания'))
+    report_from_school = models.BooleanField(default=True, verbose_name='Справка со школы')
+    medical_card = models.BooleanField(default=True, verbose_name=(u'Копия мед. карты'))
 
     def __unicode__(self):
         return self.fname + " " +  self.lname
 
 class Mark(models.Model):
-    mark = models.FloatField()
-    teacher = models.ForeignKey(Teacher)
-    date = models.DateField()
-    student = models.ForeignKey(Student)
+    mark = models.FloatField(verbose_name=u'Оценка')
+    teacher = models.ForeignKey(Teacher, verbose_name=u'Учитель')
+    date = models.DateField(verbose_name=u'Дата')
+    student = models.ForeignKey(Student, verbose_name=u'Ученик')
 
     def __unicode__(self):
         return self.student.lname + self.student.fname + ": " + str(self.mark)
