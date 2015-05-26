e40db66d4ca487f6fd114694df55eb621f159a20
Shuhrat Azimuratov <shuhrat94@ya.ru>
2015-05-26 09:56:06 +0300

    added export option

--- journal/admin.py
+++ journal/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.http import HttpResponse
 from journal.models import Teacher, Class, Subject, Mark, Student
 
+
+def export_csv(StudentAdmin, request, queryset):
+    import csv
+    from django.utils.encoding import smart_str
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=mymodel.csv'
+    writer = csv.writer(response, csv.excel)
+    response.write(u'\ufeff'.encode('utf8')) # BOM (optional...Excel needs it to open UTF-8 file properly)
+    writer.writerow([
+        smart_str(u"ID"),
+        smart_str(u"Surname"),
+        smart_str(u"Name"),
+        smart_str(u'School'),
+        smart_str(u"Class"),
+    ])
+    for obj in queryset:
+        writer.writerow([
+            smart_str(obj.pk),
+            smart_str(obj.fname),
+            smart_str(obj.lname),
+            smart_str(obj.school),
+            smart_str(obj.class_name),
+        ])
+    return response
+export_csv.short_description = u"Export CSV"
 
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ('user', 'subject')
@@ -12,6 +38,7 @@ class ClassAdmin(admin.ModelAdmin):
     list_display = ('name', 'class1_teacher')
     search_fields = ('name',)
 class StudentAdmin(admin.ModelAdmin):
+    actions = [export_csv,]
     list_display = ('lname', 'fname', 'school', 'class_name')
     search_fields = ('lname', 'fname', 'school')
 
