642f58e8b90c5ceb355385d32bbc45da11747bb2
Shuhrat Azimuratov <shuhrat94@gmail.com>
2019-04-26 11:02:31 +0300

    xlx marks export

--- journal/admin.py
+++ journal/admin.py
@@ -3,13 +3,17 @@ from django.contrib import admin
 
 # Register your models here.
 from django.http import HttpResponse
+from openpyxl.styles import Protection, Alignment
+from openpyxl.worksheet.datavalidation import DataValidation
+from openpyxl.writer.excel import save_virtual_workbook
+
 from journal.models import Teacher, Class2, Subject, Mark, Student, MarksCoeff, Parallel
 
 
 def export_xlsx(modeladmin, request, queryset):
     import openpyxl
     from django.utils.encoding import smart_str
-    from openpyxl.cell import get_column_letter
+    from openpyxl.utils import get_column_letter
     response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     response['Content-Disposition'] = 'attachment; filename=student-info.xlsx'
     wb = openpyxl.Workbook()
@@ -99,6 +103,71 @@ def export_csv(StudentAdmin, request, queryset):
 export_csv.short_description = u"Export CSV"
 
 
+def export_xls_zip_all_classes(StudentAdmin, request, queryset):
+    import openpyxl
+    from django.utils.encoding import smart_str
+    from openpyxl.utils import get_column_letter
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=journal.xlsx'
+    wb = openpyxl.Workbook()
+    wb.remove(wb.active)
+    pos = 1
+
+    columns = [
+        (u"ID", 15),
+        (u"ФИО", 20),
+        (u"Класс", 20),
+        (u"Оценка", 10),
+        (u"Комментарий", 40),
+    ]
+    dv = DataValidation(type="decimal",
+                        operator="between",
+                        formula1=0,
+                        formula2=10,
+                        allow_blank=True)
+
+    for parallel in Parallel.objects.all():
+        for group in parallel.class2_set.all():
+            isinstance(group, Class2)
+            ws = wb.create_sheet(str(parallel) + str(group.name), pos)
+            ws.protection.sheet = True
+            ws.protection.password = 'litsey7'
+            pos += 1
+            row_num = 0
+            for col_num in xrange(len(columns)):
+                c = ws.cell(row=row_num + 1, column=col_num + 1)
+                c.value = columns[col_num][0]
+
+                #c.style.font.bold = True
+                # set column width
+                ws.column_dimensions[get_column_letter(col_num + 1)].width = columns[col_num][1]
+            for student in group.student_set.all():
+
+                row_num += 1
+                row = [
+                    student.pk,
+                    smart_str(student.fname + " " + student.lname + ' ' + student.fathers_name),
+                    smart_str(str(parallel.name) + str(group.name)),
+                    smart_str(''),
+                    smart_str(''),
+                ]
+                for col_num in xrange(len(row)):
+
+                    c = ws.cell(row=row_num + 1, column=col_num + 1)
+                    c.alignment = Alignment(wrap_text=True)
+                    lock = False
+                    if col_num + 1 <= 3:
+                        lock = True
+                    c.protection = Protection(locked=lock)
+                    c.value = row[col_num]
+
+    response.write(save_virtual_workbook(wb))
+    return response
+
+
+export_xls_zip_all_classes.short_descrption = u'Export to ZIP all Classes'
+
+
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ('user', 'subject')
     search_fields = ('user', 'subject')
@@ -110,7 +179,7 @@ class ClassAdmin(admin.ModelAdmin):
 
 
 class StudentAdmin(admin.ModelAdmin):
-    actions = [export_csv, export_xlsx]
+    actions = [export_csv, export_xlsx, export_xls_zip_all_classes]
     list_display = ('fname', 'lname', 'fathers_name', 'school', 'class_name')
     search_fields = ('lname', 'fname', 'school')
 
