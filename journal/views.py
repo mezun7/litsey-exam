9e212c32ec1c93d1ea46e6d98cd5485d08e331e1
Shuhrat Azimuratov <shuhrat94@gmail.com>
2019-05-10 14:04:21 +0300

    sadasd

--- journal/views.py
+++ journal/views.py
@@ -65,7 +65,7 @@ def home(request):
     context = {'TeacherName': request.user.first_name + " " + request.user.last_name}
     teacher = Teacher.objects.all()
     teachers = teacher.filter(user=request.user)
-    return class_journal(request, teachers[0].class2_set.all().order_by('parallel','name')[0])
+    return class_journal(request, teachers[0].class2_set.all().order_by('parallel','name')[0].id)
 
     classes = get_class(teachers[0])
     # print test2[0].id
